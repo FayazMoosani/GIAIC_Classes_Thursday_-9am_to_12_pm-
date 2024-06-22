ff8e98d7cab1f9d802687173cf3bf13476591f7f
FayazMoosani <160345500+FayazMoosani@users.noreply.github.com>
2024-06-22 18:24:59 +0500

    Add files via upload

---
+++ Class_00/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// My First Assignment
+// Print "Hello, World" directly
+console.log("Hello, World"); // Outputs: Hello, World
+// Declare a variable 'message' and assign it the value "Hello, World"
+let message = "Hello, World";
+// Print the value of the variable 'message'
+console.log(message); // Outputs: Hello, World
